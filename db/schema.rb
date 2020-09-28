8b1600f08254c658fd5094ebdcea45e1f26e8ece
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-28 18:53:30 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_184739) do
+ActiveRecord::Schema.define(version: 2020_09_28_185321) do
 
   create_table "networks", force: :cascade do |t|
     t.string "call_letters"
